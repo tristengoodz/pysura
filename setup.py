47cc3bd7aec337697cee74efae4ccb9946aa2591
TheGoodz <tristen@thegoodzapp.com>
2023-03-23 10:29:19 -0500

    Updating lib data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysura",
-    version="0.9.23",
+    version="0.9.24",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
