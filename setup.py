484ec3b462ad309222a376dd78c1378782dd9825
TheGoodz <tristen@thegoodzapp.com>
2023-04-06 18:43:12 -0500

    Make it easier to redeploy the default microservice ☁️

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysura",
-    version="0.99.85",
+    version="0.99.86",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
