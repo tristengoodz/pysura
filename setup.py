8a6240e1a04d3041897993b5dd337f0d7bb2df86
TheGoodz <tristen@thegoodzapp.com>
2023-04-05 23:02:37 -0500

    Testing adding some async to speed things up 🙏

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysura",
-    version="0.99.69",
+    version="0.99.70",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
