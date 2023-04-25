465bca98643331ad8622e6d11035692a6bd062c2
TheGoodz <tristen@thegoodzapp.com>
2023-04-25 17:04:13 -0500

    Inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pysura",
-    version="0.99.141",
+    version="0.99.142",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
